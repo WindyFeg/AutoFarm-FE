f6b7efe9b3860a707f69b06f6a5869f91ca83887
WindyFeg <phongspeed7@yahoo.com.vn>
2023-03-21 00:45:35 +0700

    add basic header ui

--- src/header/index.js
+++ src/header/index.js
@@ -1,8 +1,19 @@
+import Logo from '../assets/img/AutoFarm_logo.png';
+
 function Header() {
     return (
-        <header>
-            <h1>Header</h1>
-        </header>
+        <div className="header">
+            <div className="header_logo">
+                <img src={Logo} alt="logo" className='logo' />
+            </div>
+            <div className="header_login">
+                <form>
+                    <input type="text" name="username" placeholder='Username' className='inputbox' />
+                    <input type="password" name="password" placeholder='Password' className='inputbox' />
+                    <input type="submit" value="Login" className='btn' />
+                </form>
+            </div>
+        </div>
     )
 
 }
