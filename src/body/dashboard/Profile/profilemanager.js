65c65099df064d8acc5e2adde37801a521a8e896
duync2006 <reynold.cs.w3@gmail.com>
2023-03-23 13:45:27 +0700

    modify dropdown button in profilemanager.js

--- src/body/dashboard/Profile/profilemanager.js
+++ src/body/dashboard/Profile/profilemanager.js
@@ -2,22 +2,27 @@ import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
 import { Fragment } from "react";
 import "./styles.css";
+import {useState} from 'react';
 
 function ProfileManager() {
+    const [selectedValue, setSelectedValue] = useState('');
+
+    const handleDropdownChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
 
     return (
         <Fragment>
-            <Dropdown className="dropdown">
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Default
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Automatic</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Manual</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-
+            <div className="dropdown">
+            <select value={selectedValue} onChange={handleDropdownChange}>
+                <option value= "">
+                        Default
+                </option>
+                    <option value= "Automatic"><Dropdown.Item  href="#/action-1">Automatic</Dropdown.Item></option>
+                    <option value= "Manual"><Dropdown.Item  href="#/action-2">Manual</Dropdown.Item></option>
+                    <option value= "Something else"><Dropdown.Item  href="#/action-3">Something else</Dropdown.Item></option>
+            </select>
+            </div>
             <div className="group-button mx-auto">
                 <Button id="edit-button" variant="success" size="sm">
                     Edit profile
@@ -28,7 +33,21 @@ function ProfileManager() {
             </div>
         </Fragment>
 
-    )
+    
+        // const [selectedValue, setSelectedValue] = useState('');
+      
+        // const handleDropdownChange = (event) => {
+        //   setSelectedValue(event.target.value);
+        // };
+      
+        // return (
+        //   <select value={selectedValue} onChange={handleDropdownChange}>
+        //     <option value="">--Please choose an option--</option>
+        //     <option value="option1">Option 1</option>
+        //     <option value="option2">Option 2</option>
+        //     <option value="option3">Option 3</option>
+        //   </select>
+        );
 }
 
 export default ProfileManager;
